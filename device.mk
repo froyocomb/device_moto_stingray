0c59d6b564d8863f96830e647038973360bb60fe
main3782 <101455553+main3782@users.noreply.github.com>
2026-05-24 21:43:49 +0200

    Make the compiler pick up the vendor blobs.

--- device.mk
+++ device.mk
@@ -70,3 +70,6 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/moto/stingray/media_profiles.xml:system/etc/media_profiles.xml
+
+# inherit from the non-open-source side, if present
+$(call inherit-product-if-exists, vendor/moto/stingray/stingray-vendor.mk)
